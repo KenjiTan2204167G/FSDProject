5472fb46797138a29eeee2967a7cfa2431392a06
KenjiTan <kenjitan2004@gmail.com>
2025-01-07 12:49:00 +0800

    Add project files.

---
+++ Data/TRUMPetContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TRUMPet.Domain;
+
+namespace TRUMPet.Data
+{
+    public class TRUMPetContext : DbContext
+    {
+        public TRUMPetContext (DbContextOptions<TRUMPetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TRUMPet.Domain.Artist> Artist { get; set; } = default!;
+        public DbSet<TRUMPet.Domain.Song> Song { get; set; } = default!;
+    }
+}
